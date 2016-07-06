b34990626aca4caa15c638107242fddbfd324019
Michael.s.Budde@gmail.com </ignore>
2016-07-06 10:30:51 -0700

    added animation

--- js/sketch.js
+++ js/sketch.js
@@ -1,84 +1,64 @@
 console.log('connected')
-//rows
-var rowN = 1;
-// collumn
-var ColN = 0;
-// starting block
-var block1 = 0;
-// animation direction
+
+var $game = document.getElementById('game');
+var $tRow = $game.querySelectorAll('td');
+
+var prev = 0;
+var current = 0;
+var running = true;
 var direction = 1;
-// block speed
-var speed = [475, 350, 275, 200, 150, 90, 20];
-var colTemp = 5;
-var currentTime;
-var blockArr = new Array(8);
-var pressed = false;
-var timer;
+	setInterval(function(){
+		if(running){
 
-function start()
-{
-	for (var i = 1; i <= 7; i++)
-	{
-	blockArr[i] = new Array(6);
+		$tRow[prev].classList.remove('animate');
 
-	for (var j = 1; j <= 5; j++)
-		{
-		blockArr[i][j] = document.getElementById('S' + i + j);
-		}
-	}
-
-  function colorBlock(row, col)
-  {
-  	blockArr[row][col].style.backgroundColor = 'rgb(0, 255, 0)';
-  }
-
-  function clearBlock(row, col)
-  {
-  	blockArr[row][col].style.backgroundColor = 'transparent';
-  }
+		$tRow[current].classList.add('animate');
+			prev = current;
+			current++;
+			if (current >= $tRow.length){
+				current = 0;
+				direction = 1;
+			}
 }
+	},500)
 
-function stack()
-{
-	clearTimeout(timer);
 
-	if (rowN == 1)
-	{
-		currentTime = speed[rowN];
-		rowN++;
-		block1 = colN;
-		shift();
-	}
-	else if (colN != block1)
-	{
-		lose();
-	}
-	else if (rowNumber == 7)
-	{
-		win();
-	}
-	else
-	{
-		currentTime = speed[rowN];
-		rowN++;
-		shift();
+	document.body.addEventListener("keydown", function(e){
+	  if(e.key === " "){
+	    running = !running;
+	  }
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+/
 	}
 }
-
-
-// var cells = $('#test').children();
-// console.log(cells)
-// var index = 0;
-// var keyPressed = 0;
-// while(keyPressed < 10){
-// 	setInterval(function(){
-// 	cells.eq(index).toggleClass('animate_me');
-// 	keyPressed++;
-// 	index++;
-// },100)
-// }
-
-
-// function create(){
-// 	$('#s1').toggleClass('animate_me')
-// }
+// 		})
+//
+//
+//
+// // });var keyPressed = 0;
+// // while(keyPressed < 10){
+// // }
+//
+//
+// //
+// // function create(){
+// // 	setInterval(function(){
+// // 		$('#s1').toggleClass('animate_me')
+// // },300)
+// // setInterval(function(){
+// // 	$('#s1').toggleClass('animate_me'    )
+// //
+// // },300)
+// // }
