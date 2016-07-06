aea253ba8795a72ebef4fb7941d7ab6670e27a81
Michael.s.Budde@gmail.com </ignore>
2016-07-06 16:57:53 -0700

    animation done

--- js/sketch.js
+++ js/sketch.js
@@ -1,64 +1,68 @@
 console.log('connected')
-
+var interval;
 var $game = document.getElementById('game');
-var $tRow = $game.querySelectorAll('td');
-
+var $rows = $('.row');
 var prev = 0;
 var current = 0;
 var running = true;
-var direction = 1;
-	setInterval(function(){
-		if(running){
+var speed = 200;
+// var direction = 1;
+var index = $rows.length - 1
+var $currentRow = $rows.eq(index);
 
-		$tRow[prev].classList.remove('animate');
-
-		$tRow[current].classList.add('animate');
-			prev = current;
-			current++;
-			if (current >= $tRow.length){
-				current = 0;
-				direction = 1;
-			}
-}
-	},500)
-
-
-	document.body.addEventListener("keydown", function(e){
-	  if(e.key === " "){
-	    running = !running;
-	  }
-	})
-
-
-
-
-
-
-
-
-
-
-
-
-/
+// setInterval(function(){
+// 	if(running){
+//
+//
+// 	$tRow[prev].classList.remove('animate');
+//
+// 	$tRow[current].classList.add('animate');
+// 		prev = current;
+// 		current++;
+// 		if (current >= $tRow.length){
+//
+// 			current = 0;
+//
+// 		}
+// 	}
+// },200)
+document.body.addEventListener("keydown", function(e){
+	if(e.key === " "){
+		running = !running;
+		speed - 50;
+		clearInterval(interval)
 	}
+})
+document.body.addEventListener('keyup', function(e){
+		if(e.key == ' '){
+			index--;
+			$currentRow = $rows.eq(index)
+			running = !running;
+			animateRow($currentRow.children())
+		}
+})
+function animateRow(cells){
+	// for(var i = 0;i < cells.length;i++){
+	 interval =	setInterval(function(){
+			if(running){
+				console.log(cells)
+				cells[prev].classList.remove('animate');
+				cells[current].classList.add('animate');
+				prev = current;
+				current++;
+				if (current >= cells.length){
+					current = 0;
+				}
+			} else {
+
+			}
+		},speed)
+	// }
 }
-// 		})
-//
-//
-//
-// // });var keyPressed = 0;
-// // while(keyPressed < 10){
-// // }
-//
-//
-// //
-// // function create(){
-// // 	setInterval(function(){
-// // 		$('#s1').toggleClass('animate_me')
-// // },300)
-// // setInterval(function(){
-// // 	$('#s1').toggleClass('animate_me'    )
-// //
-// // },300)
-// // }
+
+animateRow($currentRow.children());
+
+
+function win(){
+
+}
