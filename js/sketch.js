6c44037c4a4c289b8dcf1f922f9e0f7489bc107c
Michael.s.Budde@gmail.com </ignore>
2016-07-07 09:06:24 -0700

    w/e

--- js/sketch.js
+++ js/sketch.js
@@ -2,13 +2,27 @@ console.log('connected')
 var interval;
 var $game = document.getElementById('game');
 var $rows = $('.row');
+var $tds = $('td')
 var prev = 0;
 var current = 0;
 var running = true;
-var speed = 200;
+var speed = 250;
+var rowNumb = $rows.eq(6);
 // var direction = 1;
 var index = $rows.length - 1
 var $currentRow = $rows.eq(index);
+var blocksStopped = [];
+var wins = [];
+var blockArray = [];
+
+	// function check (){
+	// $('.animate').each( function(i,e) {
+	// 	 /* you can use e.id instead of $(e).attr('id') */
+	// 	 blockArray.push($(e).attr('id'));
+	// });
+	// }
+
+
 
 // setInterval(function(){
 // 	if(running){
@@ -29,15 +43,18 @@ var $currentRow = $rows.eq(index);
 document.body.addEventListener("keydown", function(e){
 	if(e.key === " "){
 		running = !running;
-		speed - 50;
+		speed -= 35;
+
 		clearInterval(interval)
 	}
 })
 document.body.addEventListener('keyup', function(e){
 		if(e.key == ' '){
 			index--;
-			$currentRow = $rows.eq(index)
+			$currentRow = $rows.eq(index);
 			running = !running;
+
+
 			animateRow($currentRow.children())
 		}
 })
@@ -45,13 +62,14 @@ function animateRow(cells){
 	// for(var i = 0;i < cells.length;i++){
 	 interval =	setInterval(function(){
 			if(running){
-				console.log(cells)
+				// console.log(cells)
 				cells[prev].classList.remove('animate');
 				cells[current].classList.add('animate');
 				prev = current;
 				current++;
 				if (current >= cells.length){
 					current = 0;
+
 				}
 			} else {
 
@@ -62,7 +80,13 @@ function animateRow(cells){
 
 animateRow($currentRow.children());
 
+if($('#s11'),$)
 
-function win(){
-
-}
+//
+// function blockCheck(){
+// 	if($tds.hasClass("animate")){
+//  			 blocksStopped.push()
+// 	}
+// }
+//
+// ()
