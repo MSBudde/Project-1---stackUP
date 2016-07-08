21e2d572752b27671b7ddbe3af4e5c6e2de988e0
MSBudde <michael.s.budde@gmail.com>
2016-07-08 09:10:34 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ document.body.addEventListener("keydown", function(e){
 	if(e.key === " "){
 		running = !running;
 		space++;
-		speed -= 29.5;
+		speed -= 31;
 		blockCheck();
 		console.log($('.animate'))
 		clearInterval(interval)
